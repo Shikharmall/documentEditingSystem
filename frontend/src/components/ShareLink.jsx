9366f52d3eaf3c6de67c2e5d4398120f08d48215
Shikhar Mall <shmall.21.2020@gmail.com>
2024-01-25 00:21:01 +0530

    solve small bug

--- frontend/src/components/ShareLink.jsx
+++ frontend/src/components/ShareLink.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import copy from "clipboard-copy";
 
 export default function ShareLink({ closeShareLink, linkText }) {
+  const [isCopied, setIsCopied] = useState(false);
   const handleCopyClick = async () => {
     try {
       await copy(linkText);
-      alert("Link copied to clipboard!");
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 1000);
+      //alert("Link copied to clipboard!");
     } catch (err) {
       console.error("Failed to copy link to clipboard", err);
     }
@@ -60,18 +65,27 @@ export default function ShareLink({ closeShareLink, linkText }) {
             <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
               <button
                 type="button"
-                class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
-                onClick={handleCopyClick}
-              >
-                Copy
-              </button>
-              <button
-                type="button"
-                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto sm:ml-3"
                 onClick={closeShareLink}
               >
                 Close
               </button>
+              {isCopied ? (
+                <button
+                  type="button"
+                  class="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                >
+                  Copied !
+                </button>
+              ) : (
+                <button
+                  type="button"
+                  class="inline-flex w-full justify-center rounded-md bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+                  onClick={handleCopyClick}
+                >
+                  Copy
+                </button>
+              )}
             </div>
           </div>
         </div>
